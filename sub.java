d621272b78e57be48bce1d31411e71590d296922
MuraliMohan <muralisocial123@gmail.com>
2024-03-24 09:05:27 +0530

    Code Committed

---
+++ sub.java
@@ -0,0 +1,13 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        // Define the two numbers
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Perform subtraction
+        int result = num1 - num2;
+        
+        // Print the result
+        System.out.println("Result of subtraction: " + result);
+    }
+}
